bd1722fc3e9103704ef9cecc279f0b609aee1096
Gaio <goncalogaio12@gmail.com>
2026-05-14 14:30:27 +0100

    added zips and modified versions to run in the ucxeption

--- frx_baseline.py
+++ frx_baseline.py
@@ -1,3 +1,5 @@
+import sys
+
 from fr1_scheduler import schedule_tasks
 from fr2_checksum import checksum
 
@@ -5,9 +7,14 @@ REPS = 1_000_000
 
 
 if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        reps = int(sys.argv[1])
+    else:
+        reps = REPS
+        
     # Same workload, prints, and REPS as frx_ft_v4.py so ucXception campaigns
     # against this baseline and the FT version are directly comparable.
-    for i in range(REPS):
+    for i in range(reps):
         result = schedule_tasks([(1, 0, 10, 50), (2, 2, 5, 50), (3, 3, 3, 50)], 0)
         print(result, flush=True)
         #result = checksum(bytes([i % 256 for i in range(10)]), 10, 0)
